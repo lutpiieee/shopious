fd7e1a2c2b04503692b4846dfbfbdc5d68dc5fdc
lutpiieee <luthfifebriyan19@gmail.com>
2024-09-18 09:33:38 +0700

    menambahkan url patterns untuk xml and json by id

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create-review-entry', create_review_entry, name='create_review_entry'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
 ]
